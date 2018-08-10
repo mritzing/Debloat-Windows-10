ed27031ca2e9e83564e5fa1cd172bdd66033b3a2
unknown <mritzing@gmail.com>
2018-08-10 12:37:07 -0400

    Laptop touchpad delay fix script

---
+++ scripts/laptopTouchpadDelay.ps1
@@ -0,0 +1,11 @@
+$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad"
+$Name = "AAPThreshold"
+$value = "0"
+IF(!(Test-Path $registryPath))
+  {
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType DWORD -Force | Out-Null}
+ ELSE {
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType DWORD -Force | Out-Null}
